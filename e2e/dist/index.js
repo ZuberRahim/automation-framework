b9dc18f5464350af076e532fd94b8bcf2ea76240
Zuber Rahim <zuber-rahim@idexx.com>
2024-07-23 12:55:25 +1200

    common config files

--- e2e/dist/index.js
+++ e2e/dist/index.js
@@ -4,6 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.smoke = exports.regression = exports.dev = void 0;
+var _dotenv = _interopRequireDefault(require("dotenv"));
+var _parseEnv = require("./env/parseEnv");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+// Load environment variables
+_dotenv.default.config({
+  path: (0, _parseEnv.env)('COMMON_CONFIG_FILE')
+});
 const common = "./src/features/**/*.feature --require-module ts-node/register --require ./src/step-definitions/**/**/*.ts -f json:./reports/report.json --format progress-bar";
 const dev = exports.dev = "".concat(common, " --tags \"@dev\"");
 const smoke = exports.smoke = "".concat(common, " --tags \"@smoke\"");
--- e2e/src/index.ts
+++ e2e/src/index.ts
@@ -1,7 +1,13 @@
+import dotenv from 'dotenv';
+import { env } from './env/parseEnv';
+
+// Load environment variables
+dotenv.config({ path: env('COMMON_CONFIG_FILE') });
+
 const common = `./src/features/**/*.feature --require-module ts-node/register --require ./src/step-definitions/**/**/*.ts -f json:./reports/report.json --format progress-bar`;
 
 const dev = `${common} --tags "@dev"`;
 const smoke = `${common} --tags "@smoke"`;
 const regression = `${common} --tags "@regression"`;
 
-export { dev, smoke, regression}
+export { dev, smoke, regression };
